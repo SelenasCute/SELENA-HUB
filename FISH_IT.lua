7d203f9e40743faa8612e974c69149e89fefe073
HonestCat <evelynperms@gmail.com>
2025-12-05 14:06:21 +0700

    s

--- FISH_IT.lua
+++ FISH_IT.lua
@@ -7,7 +7,7 @@
 local GAME = "Phoenix HUB | Fish It"
 local VERSION = 1.2
 local LATEST_UPDATE = "11/8/2025"
-local DISCORD_LINK = "dsc.gg/selena-hub"
+local DISCORD_LINK = "https://discord.gg/WcSGHZ6jjS"
 local LOGO = "rbxassetid://140413750237602"
 
 --[[===== DEPENDENCY CHECKS =====]]
@@ -179,6 +179,60 @@ local LOGO = "rbxassetid://140413750237602"
 --
 
 --[[===== UTILITY FUNCTIONS =====]]
+    local function SetDefaultTheme(WindUI)
+        WindUI:AddTheme({
+            Name = "Default",
+
+            Accent = Color3.fromHex("#ff8800"),
+            Background = Color3.fromHex("#000000"),
+            BackgroundTransparency = 0,
+            Outline = Color3.fromHex("#ffffff"),
+            Text = Color3.fromHex("#ffffff"),
+            Placeholder = Color3.fromHex("#b8b8b8"),
+            Button = Color3.fromHex("#ff7300"),
+            Icon = Color3.fromHex("#ffa54d"),
+            
+            Hover = Color3.fromHex("#ffb267"),
+
+            WindowBackground = Color3.fromHex("#101010"),
+            WindowShadow = Color3.fromHex("#000000"),
+
+            DialogBackground = Color3.fromHex("#1a1a1a"),
+            DialogBackgroundTransparency = 0,
+            DialogTitle = Color3.fromHex("#ffffff"),
+            DialogContent = Color3.fromHex("#ffffff"),
+            DialogIcon = Color3.fromHex("#ffa54d"),
+
+            WindowTopbarButtonIcon = Color3.fromHex("#ffa54d"),
+            WindowTopbarTitle = Color3.fromHex("#ffffff"),
+            WindowTopbarAuthor = Color3.fromHex("#ff8800"),
+            WindowTopbarIcon = Color3.fromHex("#ffffff"),
+
+            TabBackground = Color3.fromHex("#ff8800"),
+            TabTitle = Color3.fromHex("#ffffff"),
+            TabIcon = Color3.fromHex("#ffa54d"),
+
+            ElementTitle = Color3.fromHex("#ffffff"),
+            ElementDesc = Color3.fromHex("#ffffff"),
+            ElementIcon = Color3.fromHex("#ffa54d"),
+
+            PopupBackground = Color3.fromHex("#1a1a1a"),
+            PopupBackgroundTransparency = 0,
+            PopupTitle = Color3.fromHex("#ffffff"),
+            PopupContent = Color3.fromHex("#ffffff"),
+            PopupIcon = Color3.fromHex("#ffa54d"),
+
+            Toggle = Color3.fromHex("#ff8800"),
+            ToggleBar = Color3.fromHex("#ffffff"),
+
+            Checkbox = Color3.fromHex("#ff8800"),
+            CheckboxIcon = Color3.fromHex("#ffffff"),
+
+            Slider = Color3.fromHex("#ff8800"),
+            SliderThumb = Color3.fromHex("#ffffff"),
+        })
+
+    end
 
     local function ColorizePercent(text)
         local textPart, percent = string.match(text, "^(.-)%s*%-%s*(%d+%%)$")
@@ -311,17 +365,6 @@ local LOGO = "rbxassetid://140413750237602"
                 pchar.HumanoidRootPart.CFrame = hrp.CFrame + Vector3.new(0,5,0)
                 Notify("Teleport to "..target.Name, "Successfully Teleported to selected player", "users")
             end
-        end
-    end
-
-    local function GetGameName(placeId)
-        local success, info = pcall(function()
-            return MarketplaceService:GetProductInfo(placeId)
-        end)
-        if success and info then
-            return info.Name
-        else
-            return "Unknown Game"
         end
     end
 
@@ -753,59 +796,7 @@ local LOGO = "rbxassetid://140413750237602"
 --[[<<===== MAIN UI INITIALIZATION =====>>]]
 
     local WindUI = loadstring(game:HttpGet("https://github.com/Footagesus/WindUI/releases/latest/download/main.lua"))()
-
-    WindUI:AddTheme({
-        Name = "Default",
-
-        Accent = Color3.fromHex("#ff8800"),
-        Background = Color3.fromHex("#000000"),
-        BackgroundTransparency = 0,
-        Outline = Color3.fromHex("#ffffff"),
-        Text = Color3.fromHex("#ffffff"),
-        Placeholder = Color3.fromHex("#b8b8b8"),
-        Button = Color3.fromHex("#ff7300"),
-        Icon = Color3.fromHex("#ffa54d"),
-        
-        Hover = Color3.fromHex("#ffb267"),
-
-        WindowBackground = Color3.fromHex("#101010"),
-        WindowShadow = Color3.fromHex("#000000"),
-
-        DialogBackground = Color3.fromHex("#1a1a1a"),
-        DialogBackgroundTransparency = 0,
-        DialogTitle = Color3.fromHex("#ffffff"),
-        DialogContent = Color3.fromHex("#ffffff"),
-        DialogIcon = Color3.fromHex("#ffa54d"),
-
-        WindowTopbarButtonIcon = Color3.fromHex("#ffa54d"),
-        WindowTopbarTitle = Color3.fromHex("#ffffff"),
-        WindowTopbarAuthor = Color3.fromHex("#ff8800"),
-        WindowTopbarIcon = Color3.fromHex("#ffffff"),
-
-        TabBackground = Color3.fromHex("#ff8800"),
-        TabTitle = Color3.fromHex("#ffffff"),
-        TabIcon = Color3.fromHex("#ffa54d"),
-
-        ElementTitle = Color3.fromHex("#ffffff"),
-        ElementDesc = Color3.fromHex("#ffffff"),
-        ElementIcon = Color3.fromHex("#ffa54d"),
-
-        PopupBackground = Color3.fromHex("#1a1a1a"),
-        PopupBackgroundTransparency = 0,
-        PopupTitle = Color3.fromHex("#ffffff"),
-        PopupContent = Color3.fromHex("#ffffff"),
-        PopupIcon = Color3.fromHex("#ffa54d"),
-
-        Toggle = Color3.fromHex("#ff8800"),
-        ToggleBar = Color3.fromHex("#ffffff"),
-
-        Checkbox = Color3.fromHex("#ff8800"),
-        CheckboxIcon = Color3.fromHex("#ffffff"),
-
-        Slider = Color3.fromHex("#ff8800"),
-        SliderThumb = Color3.fromHex("#ffffff"),
-    })
-
+    SetDefaultTheme(WindUI)
 
     local Window = WindUI:CreateWindow({
         Title = GAME,
@@ -821,7 +812,7 @@ local LOGO = "rbxassetid://140413750237602"
         Transparent = false,
         Resizable = true,
         SideBarWidth = 150,
-        Theme = "test",
+        Theme = "Default",
         Radius = 16,
         ElementsRadius = 10,
     })
@@ -1830,6 +1821,4 @@ local LOGO = "rbxassetid://140413750237602"
 --[[===== FINALIZE =====]]
 print("────────────────────────────")
 print("💉 Phoenix HUB Executed Successfully")
-print("Game: "..GetGameName(game.PlaceId).." | Version: "..VERSION)
-print("Status: Modules Loaded, UI Initialized ✅")
 print("────────────────────────────")
